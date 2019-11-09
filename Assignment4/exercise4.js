3cb71f9a07a8fd65cfa7196902e0673a8c01c24c
Mike <mmcgrath@psu.edu>
2019-11-08 23:32:33 -0800

    added height and width console.log()

---
+++ Assignment4/exercise4.js
@@ -0,0 +1,10 @@
+var height = document.getElementById("height").clientHeight;
+var width = document.getElementById('width').clientWidth;
+
+function size(){
+    var height = document.getElementById("height").clientHeight;
+    var width = document.getElementById('width').clientWidth;
+
+    console.log("Height: " + height);
+    console.log("Width: " + width);
+}
