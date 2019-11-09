64a74f65a6e7c06849ca2cdb6a6ff414de220bdc
Mike <mmcgrath@psu.edu>
2019-11-08 22:45:11 -0800

    added the console.log() button update

---
+++ Assignment4/exercise3.js
@@ -0,0 +1,9 @@
+
+function nameChange() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var comment = document.getElementById("comment").value;
+    console.log("Name: " + name);
+    console.log("Email: " + email);
+    console.log("Message " + comment);
+}
