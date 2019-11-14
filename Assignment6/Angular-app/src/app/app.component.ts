1e5d2f6766609730916b0c0ac7b288a30011a83e
Mike <mmcgrath@psu.edu>
2019-11-14 09:33:39 -0800

    double checking all is pushed

---
+++ Assignment6/Angular-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h1>Hello I'm an Angular app!</h1>
+  <h2>Very soon you will learn how to create and bootstrap me!</h2>
+`
+
+})
+export class AppComponent {
+  title = 'Angular-app';
+}
